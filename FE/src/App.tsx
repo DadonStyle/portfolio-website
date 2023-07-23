4f0a21534c66256eb03b961c7cbb7450e1ca5c03
Noam <noamoni9@gmail.com>
2023-07-23 11:45:28 +0300

    todo and add console.log

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -11,6 +11,19 @@ const App = () => {
   const cursor = useMousePosition();
   return (
     <>
+      {console.log(
+        "%c,-.___,-.\r \n\\_/_ _\\_/\r\n  )O_O(\r\n { (_) }\r\n  `-^-'",
+        'color:brown; font-size: x-large;'
+      )}
+      {console.log(
+        '%c Hey!, Meet my dog Juno',
+        'color:brown; font-size: medium;'
+      )}
+      {console.log(
+        '%c Welcome and thank you for looking at my web!',
+        'color:brown; font-size: medium;'
+      )}
+
       <S.MainContainer>
         {cursor}
         <Background />
